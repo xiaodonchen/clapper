2d1f7ed6393eea85443c64308340131a660748ec
cailan <cailan@beihai365.com>
2020-06-07 23:25:57 +0800

    提交一波

--- js/index.js
+++ js/index.js
@@ -1,20 +1,10 @@
-var video = document.getElementById('pc-video');
+
 $(function(){
-    $(".do-list ul li").mouseenter(function(){
-        $(this).find('.do-img').addClass('hide')
-        $(this).find('.do-img-h').removeClass('hide')
-        // $('.imgShow .bigImg').attr('src', $(this).find('img').attr('src'));
-    }).mouseleave( function(){
-        $(this).find('.do-img').removeClass('hide')
-        $(this).find('.do-img-h').addClass('hide')
-    });
+    
 	$('.j-sle').click(function(){
+        console.log(2);
 		$('.right-sel').toggle();
 	})
 	 
-	if($('.pc-video').css("display") == "none"){
-		video.pause();
-	}else{
-		 video.play();
-	}
+	
 });
