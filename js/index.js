528e1a979ba2f6f6ea3f7013cb0d3c4f6a13a79d
baixiong <baixiong@beihai365.com>
2020-06-08 21:09:17 +0800

    修复

--- js/index.js
+++ js/index.js
@@ -5,6 +5,12 @@ $(function(){
         $('.right-sel').toggle();
         $('.right-sel').is(":hidden") ? $('body').css('overflow','auto') : $('body').css('overflow','hidden');
 	})
-	 
+        $(".j-img-h").mouseenter(function(){
+                $(this).find('.no-h').addClass('hide')
+                $(this).find('.h').removeClass('hide')
+        }).mouseleave( function(){
+                $(this).find('.no-h').removeClass('hide')
+                $(this).find('.h').addClass('hide')
+        });
 	
 });
